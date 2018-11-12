1cea60d41643d4f05d243732199dd81ca0006e6b
Yadav <priyalagrawal0810@gmail.com>
2018-11-12 00:11:53 -0800

    Initial commit

---
+++ Source/Assets/Scripts/HelpCommand.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpCommand : Command
+{
+
+    public override void Execute() {
+    }
+}
---
+++ Source/Assets/Scripts/Menu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Menu : MonoBehaviour, IMenu
+{
+    Command quitCommand = new QuitCommand();
+    public void Play()
+    {
+        //Application.loadedLevel()
+    }
+
+    public void Help()
+    {
+        //Application.LoadLevel("Help");
+    }
+
+    public void Settings()
+    {
+        //Application.LoadLevel("Settings");
+    }
+
+    public void Quit()
+    {
+       quitCommand.Execute();
+    }
+}
