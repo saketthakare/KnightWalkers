3eeb0c4ded5d1ed83311d5316242bc2dee0642b2
Yadav <priyalagrawal0810@gmail.com>
2018-11-19 00:27:57 -0800

    Global music controller added with singleton pattern + Audio handling done properly

---
+++ Codebase/Assets/Scripts/MainMenu/AudioController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioController : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public Slider volumeSlider;
+    public Toggle musicToggler;
+    private readonly GlobalAudioController globalAudioController = GlobalAudioController.GetInstance();
+
+    void Start(){
+        if (audioSource != null)
+        {
+            audioSource.enabled = globalAudioController.GetMusicOption();
+            audioSource.volume = globalAudioController.GetVolume();
+        }
+        if (volumeSlider != null)
+        {
+            volumeSlider.value = globalAudioController.GetVolume();
+            volumeSlider.onValueChanged.AddListener(delegate { ControlVolume(); });
+        }
+        if (musicToggler != null)
+        {
+            musicToggler.onValueChanged.AddListener(delegate { ToggleAudio(); });
+        }
+    }
+
+    private void ToggleAudio()
+    {
+        audioSource.enabled = !audioSource.enabled;
+        /*if (audioSource.volume.Equals(0.0f))
+        {
+            audioSource.volume = globalAudioController.GetVolume();
+        }
+        else
+            audioSource.volume = 0.0f;*/
+        globalAudioController.SetMusicOption(audioSource.enabled);
+    }
+
+    private void ControlVolume()
+    {
+        audioSource.volume = volumeSlider.value;
+        globalAudioController.SetVolume(audioSource.volume);
+    }
+}
---
+++ Codebase/Assets/Scripts/MainMenu/GlobalAudioController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalAudioController
+{
+   private float volume;
+    private bool musicOption;
+    private static GlobalAudioController instance = null;
+
+    private GlobalAudioController()
+    {
+        volume = 0.02f;
+        musicOption = true;
+    }
+
+    public static GlobalAudioController GetInstance()
+    {
+        if (instance == null)
+            instance = new GlobalAudioController();
+        return instance;
+    }
+
+    public float GetVolume()
+    {
+        return volume;
+    }
+
+    public bool GetMusicOption()
+    {
+        return musicOption;
+    }
+
+    public void SetVolume(float vol)
+    {
+        volume = vol;
+    }
+
+    public void SetMusicOption(bool option)
+    {
+        musicOption = option;
+    }
+}
