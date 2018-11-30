54bbdabf0217047561608bd9aa3da86e21f2d46e
Saket Thakare <saketthakare@gmail.com>
2018-11-30 10:48:52 -0800

    Update to pipe result output

--- Leaderboard/score.controller.js
+++ Leaderboard/score.controller.js
@@ -3,45 +3,48 @@
 const httpStatus = require('http-status')
 const sql = require('./database-connection')
 
-
 exports.addScore = async (req, res, next) => {
     try {
-        if(!req.body.name || !req.body.score) { throw 'Invalid Data' }
-        const response = {payLoad: {}}
+        if(!req.params.name || !req.params.score) { throw 'Invalid Data' }
         const newScore = {
-          'name': req.body.name,
-          'score': req.body.score
+          'name': req.params.name,
+          'score': parseInt(req.params.score)
         }
         const currentScore = await sql.query('INSERT INTO score SET ?', newScore)
+        res.setHeader('content-type', 'text/plain');
+        res.status(httpStatus.OK)
+        res.send(''+currentScore.insertId)
+      } catch (error) {
+        res.status(httpStatus.INTERNAL_SERVER_ERROR)
+        res.send('error')
+      }
+}
+
+exports.fetchScore = async (req, res, next) => {
+    try {
+        if(!req.params.id) { throw 'Invalid Data' }
+        const id = parseInt(req.params.id)
         const allScore = await sql.query(`SELECT * FROM score ORDER BY score DESC`)
-        const currentRun = {
-            id : currentScore.insertId,
-            name : '',
-            score : '',
-            rank : 0
-        }
-        const leaderboardList = []
+        let leaderboardList = ''
         for (let index = 0; index < allScore.length; index++) {
             const element = allScore[index]
             if(index < 5){
                 const player = element
                 player.rank = index + 1
-                leaderboardList.push(player)
+                leaderboardList += player.id+'|'+player.name+'|'+player.score+'|'+player.rank+'\n'
             }
-            if(element.id == currentScore.insertId){
-                currentRun.name = element.name
-                currentRun.score = element.score
-                currentRun.rank = index + 1
+            if(element.id == id){
+                const player = element
+                player.rank = index + 1
+                leaderboardList = id+'|'+player.name+'|'+player.score+'|'+player.rank+'\n' + leaderboardList
             }            
         }
-        response.payLoad = {
-            current : currentRun,
-            leaderboard : leaderboardList
-        }
+        res.setHeader('content-type', 'text/plain');
         res.status(httpStatus.OK)
-        res.send(response)
+        res.send(leaderboardList)
       } catch (error) {
+          console.error(error)
         res.status(httpStatus.INTERNAL_SERVER_ERROR)
-        res.send(response)
+        res.send('error')
       }
 }
